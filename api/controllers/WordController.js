1f62c273ea5f99cb30adb807d1e38dfc126e34c4
bnguyen86 <bnguyen@outlook.com>
2016-11-30 23:06:56 -0800

    Started new project

---
+++ api/controllers/WordController.js
@@ -0,0 +1,11 @@
+/**
+ * WordController
+ *
+ * @description :: Server-side logic for managing Words
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
